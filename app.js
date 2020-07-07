368969eb6ac11e21e1d2217867628e02a7645e53
matheus.oliveira <matheusoliveira.1641@gmail.com>
2020-07-07 19:44:29 -0300

    Encapsula variáveis de ambiente com .env

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import studentRouter from './routes/studentRouter.js';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // Conectar ao MongoDB usando Mongoose
 (async () => {
   try {
     await mongoose.connect(
-      'mongodb+srv://adm:igtibootcamp@cluster0.3xpjn.mongodb.net/grades?retryWrites=true&w=majority',
+      `mongodb+srv://${process.env.USERDB}:${process.env.USERDBPASSWORD}@cluster0.3xpjn.mongodb.net/grades?retryWrites=true&w=majority`,
       { useNewUrlParser: true, useUnifiedTopology: true }
     );
   } catch (error) {
@@ -20,4 +23,4 @@ const app = express();
 app.use(express.json());
 app.use('/student', studentRouter);
 
-app.listen(3000, () => console.log('API started'));
+app.listen(process.env.PORT, () => console.log('API started'));
